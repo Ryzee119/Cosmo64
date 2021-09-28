1791d9d1663e18e29a4c03e26e68b4545aa21bde
Ryzee119 <wendland@live.com.au>
2021-09-28 14:25:11 +0930

    Audio: Update audio backend during delays

--- n64/SDL/SDL_timer.h
+++ n64/SDL/SDL_timer.h
@@ -11,7 +11,16 @@ static inline Uint32 SDL_GetTicks()
 
 static inline void SDL_Delay(Uint32 ms)
 {
-    return wait_ms(ms);
+    unsigned int initial_tick = TICKS_READ();
+    do
+    {
+        if (audio_can_write())
+        {
+            short *buf = audio_write_begin();
+            mixer_poll(buf, audio_get_buffer_length());
+            audio_write_end();
+        }
+    } while (TICKS_READ() - initial_tick < TICKS_FROM_MS(ms));
 }
 
 #endif
